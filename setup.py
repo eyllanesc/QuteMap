2a71ff75f4a2ae721d981a9475fb3db2b22b71f4
eyllanesc <e.yllanescucho@gmail.com>
2019-06-16 02:38:15 -0500

    travis

--- setup.py
+++ setup.py
@@ -25,7 +25,6 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Natural Language :: English',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
     ],
